bf141fbd802ac9aa911294c0cd712bd2481fb095
Rishi <kukadiyarishi895@gmail.com>
2025-05-10 18:45:26 +0530

    First Navbar completed

---
+++ Froentend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import reactDom from "react-dom/client";
+import Navbar from "./components/navbar";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+const App = () => {
+  return (
+        <>
+        <Navbar></Navbar>
+        </>
+    )
+}
+
+export default App
---
+++ Froentend/src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../CSS/navbar.css";
+import Logo from "../assets/yajveer-logo.jpg";
+export default function Navbar() {
+      return (
+        <nav className="navbar">
+          <div className="navbar-left">
+            <p>Become A Seller</p>
+            <p>About Us</p>
+            <p>Free Delivery</p>
+            <p>Returns Policy</p>
+          </div>
+          <div className="navbar-right">
+            <div className="navbar-dropdown">
+              <p>Help Center</p>
+              <ul className="dropdown-menu">
+                <li>
+                  <i className="bi bi-headset"></i> Call Center
+                </li>
+                <li>
+                  <i className="bi bi-chat-dots"></i> Live Chat
+                </li>
+              </ul>
+            </div>
+            <i className="bi bi-person-circle"></i>
+            <button className="account-button">My Account</button>
+          </div>
+        </nav>
+      );
+}
