f5e2d9862e5695f62106ce7475c05e1f17984719
Rishi <kukadiyarishi895@gmail.com>
2025-05-13 18:08:15 +0530

    Sign In Done

--- Froentend/src/components/login.jsx
+++ Froentend/src/components/login.jsx
@@ -1,9 +1,43 @@
 import "../CSS/login.css";
+import Navbar from './navbar';
+import Navbar2 from './navbar2';
+import Ayur from "../assets/logp.jpg";
 export default function Login(){
     return(
         <>
+            <Navbar></Navbar>
+            <Navbar2></Navbar2>
             <div className="log">
-
+                    <div className="imgsec">
+                        <img src={Ayur} alt="Yajveer" />
+                    </div>
+                    <div className="logform ">
+                        <div className="mainlog">
+                            <div className="wel">
+                                <p className="logn">Login</p>
+                                <p>Welcome to Yajveer!</p>
+                                <p> Please Login to your account</p>
+                            </div>
+                            <div className="field">
+                                <form action="" className="logf">
+                                    <div className="usn">
+                                            <label htmlFor="userName">User Name : </label>
+                                     <input type="text"id="userName" name="userName" placeholder="Enter Your UserName"/>
+                                    </div>
+                                    <div className="usp">
+                                             <label htmlFor="password">Password : </label>
+                                     <input type="password" id="password" name="password" placeholder="Enter Your Password"></input>
+                                    </div>
+                                    <p>Forgot Your Password</p>
+                                    <button>Login</button>
+                                </form>
+                                <div className="newus">
+                                    <p className="ne">New User ? </p>
+                                    <p className="ne1">SignUp</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </>
     )
