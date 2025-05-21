93ecf608c8ff3bc46ce3b96ae500928e958c7291
Rishi <kukadiyarishi895@gmail.com>
2025-05-21 22:23:50 +0530

    User schema added

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([\.+-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      trim: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: [true, "Mobile number is required"],
+      trim: true,
+      match: [/^[6-9]\d{9}$/, "Please enter a valid 10-digit mobile number"],
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+userSchema.index({ email: 1 });
+
+export const User = mongoose.model("User", userSchema);
